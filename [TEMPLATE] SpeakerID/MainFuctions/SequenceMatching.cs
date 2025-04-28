4511b3b70d20f59030f63b7947eb151cf41e5c23
devfadyhany <dev.fadyhany@gmail.com>
2025-04-28 21:37:20 +0300

    setup inital code template.

---
+++ [TEMPLATE] SpeakerID/MainFuctions/SequenceMatching.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Recorder.MFCC;
+
+namespace Recorder
+{
+    class SequenceMatching
+    {
+        public static int DTW_NoPruning(Sequence input, Sequence template, int N, int M)
+        {
+            int dissimilarityScore = 0; // Total Distance
+
+            // TODO: Implement Matching Algorithm without Pruning.
+
+            return dissimilarityScore;
+        }
+
+        public static int DTW_Pruning(Sequence input, Sequence template, int N, int M, int pruningWidth)
+        {
+            int dissimilarityScore = 0; // Total Distance
+
+            // TODO: Implement Matching Algorithm with Pruning.
+
+            return dissimilarityScore;
+        }
+
+        #region BonusFunctions
+        public static void Sync_Search()
+        {
+            // TODO: Implement Time Synchronous Search.
+        }
+
+        public static void BeamSearch()
+        {
+            // TODO: Implement Pruning by limiting Path Cost (Beam Search).
+        }
+
+        public static void Sync_BeamSearch()
+        {
+            // TODO: Implement Time Synchronous Beam Search.
+        }
+        #endregion
+
+        #region HelperFunctions
+
+        #endregion
+    }
+}
---
+++ [TEMPLATE] SpeakerID/MainFuctions/UserIdentification.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recorder
+{
+    class UserIdentification
+    {
+        /*
+         * Inputs
+         * ==========================================
+         * 1.Labeled template sequences (i.e. a name associated with each sequence).
+         * 2.New unknown input sequence.
+         * 3.Pruning width (W).
+         * 
+         * Outputs
+         * ==========================================
+         * 1.Label of the unknown input sequence.
+         * 2.Final cost of the matched sequence.
+         */
+
+
+        /*
+         * TODO: Implement Enrollment Phase Functions
+         * ==========================================
+         * New users can enroll (register) themselves by recording a few words,
+         * and typing their name at the end.
+         */
+
+        /*
+         * TODO: Implement Identification Phase Functions
+         * ==========================================
+         * The program can identify an existing user 
+         * by recording a new speech segment for the user,
+         * and by applying the sequence matching algorithm
+         * between the input sequence and the entire pre-existing template sequences,
+         * the program should determine the name of the user and output it on the screen.
+         */
+    }
+}
