e530c29052e32d2cf36a13bd098e3acd42a500ec
devfadyhany <dev.fadyhany@gmail.com>
2025-04-29 16:14:12 +0300

    implement initial solution for DTW_NoPruning

--- [TEMPLATE] SpeakerID/MainFuctions/SequenceMatching.cs
+++ [TEMPLATE] SpeakerID/MainFuctions/SequenceMatching.cs
@@ -10,11 +10,41 @@ namespace Recorder
     {
         public static int DTW_NoPruning(Sequence input, Sequence template, int N, int M)
         {
-            int dissimilarityScore = 0; // Total Distance
-
             // TODO: Implement Matching Algorithm without Pruning.
+            int[,] dissimilarityMatrix = new int[N + 1, M + 1];
 
-            return dissimilarityScore;
+            for (int i = 0; i <= N; i++)
+            {
+                if (i == 0)
+                    dissimilarityMatrix[0, i] = 0;
+                else
+                    dissimilarityMatrix[0, i] = dissimilarityMatrix[0, i - 1] + 1;
+            }
+
+            for (int i = 0; i <= M; i++)
+            {
+                dissimilarityMatrix[i, 0] = dissimilarityMatrix[i - 1, 0] + 1;
+            }
+
+            if (N == 0 || M == 0)
+                return dissimilarityMatrix[N, M];
+
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= M; j++)
+                {
+                    int insertCost = dissimilarityMatrix[i, j - 1] + 1;
+                    int deleteCost = dissimilarityMatrix[i - 1, j] + 1;
+                    int replaceCost = dissimilarityMatrix[i - 1, j - 1];
+
+                    if (FramesMatched(input.Frames[i - 1], template.Frames[j - 1]))
+                        replaceCost++;
+
+                    dissimilarityMatrix[i, j] = Math.Min(Math.Min(insertCost, deleteCost), replaceCost);
+                }
+            }
+
+            return dissimilarityMatrix[N, M];
         }
 
         public static int DTW_Pruning(Sequence input, Sequence template, int N, int M, int pruningWidth)
@@ -44,6 +74,16 @@ namespace Recorder
         #endregion
 
         #region HelperFunctions
+        public static bool FramesMatched(MFCCFrame frame1, MFCCFrame frame2)
+        {
+            for (int i = 0; i < 13; i++)
+            {
+                if (frame1.Features[i] != frame2.Features[i])
+                    return false;
+            }
+
+            return true;
+        }
 
         #endregion
     }
